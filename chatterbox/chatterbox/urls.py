aaef787da08c11091edc5331afdf84ce3a110154
pawo161 <pawo161@gmail.com>
2019-11-18 12:49:03 +0100

    change

--- chatterbox/chatterbox/urls.py
+++ chatterbox/chatterbox/urls.py
@@ -1,7 +1,4 @@
-"""comedy URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+"""
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +12,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from comedy.comedy.views import home, get_response
-from comedy.views import home, get_response
+
+from chatterbox.views import home, get_response
 
 from django.conf.urls.static import static
 from django.conf import settings
